a5426e0af64b69623c5b309306f15621529e7c8f
Sean Myers <iAmSeanMyers@gmail.com>
2021-10-14 12:52:36 -0700

    wrote a simple madlib script for a funny little story

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Madlibs-simple
+
+Simple madlib python script as starter just to try out 
+There are 5 Adjectives, 3 Nouns, 2 Past tense verbs an adverb and a verb for variables
---
+++ madlib.py
@@ -0,0 +1,22 @@
+adj = input("Adjective: ")
+adj2 = input("Adjective: ")
+adj3 = input("Adjective: ")
+adj4 = input("Adjective: ")
+adj5 = input("Adjective: ")
+noun = input("Noun: ")
+noun2 = input("Noun: ")
+noun3 = input("Noun: ")
+past_verb = input("Verb, past tense: ")
+past_verb2 = input("Verb, past tense: ")
+adverb = input("Adverb: ")
+verb = input("Verb: ")
+
+
+
+madlib = f"Today I went to the zoo. I saw a(n) {adj} {noun} jumping up and down in its tree. \
+He {past_verb} {adverb} through the large tunnel that led to its {adj2} {noun2}. \
+I got some peanuts and passed them through the cage to a gigantic gray {noun3} towering above my head. Feeding that animal made me hungry.  \
+I went to get a {adj3} scoop of ice cream. It filled my stomach. Afterwards I had to {verb} {adj4} to catch our bus. When I got home I {past_verb2} \
+my mom for a {adj5} day at the zoo."
+
+print(madlib)
